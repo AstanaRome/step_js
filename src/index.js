9a27ca173ceb14d8129682ea6e380c0105c35990
ShalabayevAlikhan <alihan_3991@mail.ru>
2021-10-11 03:38:36 +0600

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+import '../assets/style.scss';
+
 const axios = require('axios');
 const cat = document.getElementById('cat_img');
 
 
 axios.get('https://aws.random.cat/meow')
 
-.then(response => {
-const data = response.data.file;
-console.log(data);
-cat.src = data;
-})
+    .then(response => {
+        const data = response.data.file;
+        console.log(data);
+        cat.src = data;
+    })
+
+
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    devServer: {
+        port: 8080,
+        compress: true,
+        static: {
+            directory: './public',
+        },
+    },
+
+
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+        ],
+    }
+
+};
