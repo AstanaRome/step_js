14c80d1b3f20c9f60718575a95754dea9d03d342
ShalabayevAlikhan <alihan_3991@mail.ru>
2021-10-19 01:10:22 +0600

    commit_1910

---
+++ src/components/user-info.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main-page">
+     <h1>
+      Онлайн кинотеатр
+    </h1>
+
+<button type="button" class="btn btn-outline-light" @click="$router.push({ name: 'films' })">Перейти к фильмам</button>
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ffff',
+  data() {
+    return {
+      text: 'Hello world!',
+      
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .main-page {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    h1 {
+      color: #ffffff;
+      font-size: 70px;
+      margin-bottom: 20px;
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
+import VueRouter from 'vue-router'
 
+import '@/assets/styles/style.scss';
 Vue.config.productionTip = false
-
+Vue.use(VueRouter)
 new Vue({
   router,
   render: h => h(App)
