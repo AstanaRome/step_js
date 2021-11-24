4b0783b4a431356bbdd7df192b9a95b8e5f4a0fb
ShalabayevAlikhan <alihan_3991@mail.ru>
2021-11-24 10:33:28 +0600

    add detail

--- src/views/DetailRecord.vue
+++ src/views/DetailRecord.vue
@@ -1,33 +1,62 @@
 <template>
   <div>
-    <div class="app-main-layout">
-      <div>
-        <div>
-          <div class="breadcrumb-wrap">
-            <a href="/history" class="breadcrumb">История</a>
-            <a class="breadcrumb"> Расход </a>
-          </div>
-          <div class="row">
-            <div class="col s12 m6">
-              <div class="card red">
-                <div class="card-content white-text">
-                  <p>Описание:</p>
-                  <p>Сумма:</p>
-                  <p>Категория:</p>
+    <Loader v-if="loading"/>
+    <div v-else-if="record">
+      <div class="breadcrumb-wrap">
+        <router-link to="/history" class="breadcrumb">История</router-link>
+        <a
+          @click.prevent
+          class="breadcrumb"
+        >{{ record.type === 'income' ? 'Доход' : 'Расход' }}</a>
+      </div>
+      <div class="row">
+        <div class="col s12 m6">
+          <div
+            class="card"
+            :class="{
+            'red': record.type === 'outcome',
+            'green': record.type === 'income'
+          }"
+          >
+            <div class="card-content white-text">
+              <p>Описание: {{record.description}}</p>
+              <p>Сумма: {{record.amount}}</p>
+              <p>Категория: {{record.categoryName}}</p>
 
-                  <small>12.12.12</small>
-                </div>
-              </div>
+              <small>{{record.date | date('datetime')}}</small>
             </div>
           </div>
         </div>
       </div>
-
-      <div class="fixed-action-btn">
-        <a class="btn-floating btn-large blue" href="#">
-          <i class="large material-icons">add</i>
-        </a>
-      </div>
     </div>
+    <p class="center" v-else>Запись с id={{$route.params.id}} не найдена</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'detail',
+  metaInfo () {
+    return {
+      title: this.$title('Detail_Title')
+    }
+  },
+  data: () => ({
+    record: null,
+    loading: true
+  }),
+  async mounted () {
+    const id = this.$route.params.id
+    const record = await this.$store.dispatch('fetchRecordById', id)
+    const category = await this.$store.dispatch(
+      'fetchCategoryById',
+      record.categoryId
+    )
+    this.record = {
+      ...record,
+      categoryName: category.title
+    }
+    this.loading = false
+  }
+}
+</script>
